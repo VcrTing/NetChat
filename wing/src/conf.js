64ae7d42e81fdb9eb7c26997cd2544f2df07e198
VcrTing <vcrting@163.com>
2022-11-28 13:13:21 +0800

    0

--- wing/src/conf.js
+++ wing/src/conf.js
@@ -1,8 +1,8 @@
-const TEST = true
+const TEST = false
 
 const BASE = 'https://strapi10.svr.up5d.com'
 
-const SPEED = TEST ? 8000 * 1000 : 8 * 1000
+const SPEED = TEST ? 8000 * 1000 : 5 * 1000
 
 import ENDPOINT from './air/endpoint'
 import PLUGINS_CONF from './air/plugins_conf'
